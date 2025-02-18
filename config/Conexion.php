db17f292006e965da00325a102f5db73eec0ff06
Sara Garzón <saragarzongaldeano@gmail.com>
2025-02-18 11:08:23 +0100

    conexionBaseDatos

--- config/Conexion.php
+++ config/Conexion.php
@@ -2,7 +2,7 @@
 class Conexion{
     public static function Conectar() {
         define('servidor', 'localhost');
-        define('nombre_bd', 'Saratienda');
+        define('nombre_bd', 'tiendasara');
         define('usuario', 'root');
         define('password', '');
 
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+require_once 'config/Conexion.php';
+
+$conexion = Conexion::Conectar();
+?>
