4f656039141b9c1579d2b3479393dc29dc577a60
Sara Garzón <saragarzongaldeano@gmail.com>
2025-02-19 12:50:22 +0100

    Conexion Arreglada

--- config/Conexion.php
+++ config/Conexion.php
@@ -1,4 +1,7 @@
 <?php
+
+namespace Config;
+
 class Conexion{
     public static function Conectar() {
         define('servidor', 'localhost');
@@ -6,12 +9,12 @@ class Conexion{
         define('usuario', 'root');
         define('password', '');
 
-        $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+        $opciones = array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
         try {
-            $conexion = new PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
+            $conexion = new \PDO("mysql:host=".servidor."; dbname=".nombre_bd, usuario, password, $opciones);
             return $conexion;
-        } catch (Exception $e) {
+        } catch (\Exception $e) {
             die("El error de Conexión es: ". $e->getMessage());
         }
     }
