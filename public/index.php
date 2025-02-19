88c68b6279dfcb89ecf76fdda3b47f132f4de080
Sara Garzón <saragarzongaldeano@gmail.com>
2025-02-19 13:27:29 +0100

    Comprobacion conexion

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
-require_once 'config/Conexion.php';
+require_once '../config/Conexion.php';
+
+use Config\Conexion;
 
 $conexion = Conexion::Conectar();
+
+if ($conexion) {
+    echo "Conexión exitosa";
+} else {
+    echo "Error en la conexión";
+}
 ?>
