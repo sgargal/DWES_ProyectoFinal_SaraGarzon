1d4028bd1bafbb7e6f160847de257db440412d86
Sara Garzón <saragarzongaldeano@gmail.com>
2025-02-24 14:22:53 +0100

    empezando css(poner mas bonito)

--- app/views/usuario/formularioRegistro.php
+++ app/views/usuario/formularioRegistro.php
@@ -7,6 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Registro</title>
+    <link rel="stylesheet" href="../../../css/style.css">
 </head>
 <body>
     <h1>REGISTRO DE USUARIO</h1>
